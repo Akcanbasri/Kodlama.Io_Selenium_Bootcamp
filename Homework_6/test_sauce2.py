c7b6acd3b8b7bc2b07c1ed440683fea4eb6978d4
Hasan Basri <77155291+Akcanbasri@users.noreply.github.com>
2023-03-29 23:24:15 +0300

    29.03.2023 Homework 6 ends

--- Homework_6/test_sauce2.py
+++ Homework_6/test_sauce2.py
@@ -1,3 +1,4 @@
+# libraries which will we need
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -6,10 +7,12 @@ import pytest
 from datetime import date
 from pathlib import Path
 from selenium.webdriver.common.action_chains import ActionChains
-from time import sleep
 from Homework_6.Constants import constants
+from selenium.webdriver.support.ui import Select
+from time import sleep
 
 
+# Class name
 class Test_sauce:
     # calls before test
     def setup_method(self):
@@ -23,9 +26,11 @@ class Test_sauce:
     def teardown_method(self):
         self.driver.quit()
 
+    # wait method for all tests
     def wait_for_element_visible(self, locator, timeout=5):
         WebDriverWait(self.driver, timeout).until(ec.visibility_of_element_located(locator))
 
+    # testing about site's about page
     def test_about(self):
         self.test_valid_login("standard_user", "secret_sauce")
 
@@ -41,6 +46,7 @@ class Test_sauce:
 
         assert self.driver.current_url == constants.saucelab_about_url
 
+    # testing about banned account
     @pytest.mark.parametrize("username,password", [("locked_out_user", "secret_sauce")])
     def test_banned_account(self, username, password):
         self.wait_for_element_visible((By.ID, constants.user_id))
@@ -65,6 +71,7 @@ class Test_sauce:
 
         assert error_msg.text == constants.banned_account_message
 
+    # testing about invalid login
     @pytest.mark.parametrize("username,password", [("1", "1"), ("kodlama.io", "123")])
     def test_invalid_login(self, username, password):
         self.wait_for_element_visible((By.ID, constants.user_id))
@@ -89,6 +96,7 @@ class Test_sauce:
 
         assert error_msg.text == constants.unmatched_username_password_text
 
+    # testing about login pace only click to login button
     def test_just_click_login(self):
         self.wait_for_element_visible((By.ID, constants.login_id))
         login_btn = self.driver.find_element(By.ID, constants.login_id)
@@ -102,6 +110,7 @@ class Test_sauce:
 
         assert error_msg.text == constants.just_clik_message
 
+    # testing about login with valid information
     @pytest.mark.parametrize("username,password", [("standard_user", "secret_sauce")])
     def test_valid_login(self, username, password):
         self.wait_for_element_visible((By.ID, constants.user_id))
@@ -121,6 +130,7 @@ class Test_sauce:
 
         assert self.driver.current_url == constants.product_list_url
 
+    # testing about logout
     def test_logout(self):
         self.test_valid_login("standard_user", "secret_sauce")
 
@@ -138,6 +148,7 @@ class Test_sauce:
 
         assert self.driver.current_url == constants.URL
 
+    # Testing for login only using username
     @pytest.mark.parametrize("username", [("1"), ("kodlama.io")])
     def test_only_username(self, username):
         self.wait_for_element_visible((By.ID, constants.user_id))
@@ -155,6 +166,7 @@ class Test_sauce:
                                         "//h3[contains(.,\'Epic sadface: Password is required\')]"
                                         "").text == constants.only_username_message
 
+    # Testing about problem user
     @pytest.mark.parametrize("username,password", [("problem_user", "secret_sauce")])
     def test_problem_user(self, username, password):
         self.wait_for_element_visible((By.ID, constants.user_id))
@@ -175,6 +187,7 @@ class Test_sauce:
 
         assert self.driver.current_url == constants.product_list_url
 
+    # testing for total product
     def test_counting_products(self):
         total_product = 6
         self.test_valid_login("standard_user", "secret_sauce")
@@ -190,6 +203,7 @@ class Test_sauce:
 
         assert len(list_product) == total_product
 
+    # Testing for 'x' icon for invalid logins
     def test_x_icon(self):
         self.wait_for_element_visible((By.ID, constants.login_id))
         login_button = self.driver.find_element(By.ID, constants.login_id)
@@ -204,6 +218,7 @@ class Test_sauce:
 
         assert error_icon > 0
 
+    # test for sorting products from z to a
     def test_sort_product_z_to_a(self):
         self.test_valid_login("standard_user", "secret_sauce")
         name_of_product_before = list()
@@ -233,6 +248,7 @@ class Test_sauce:
 
         assert name_of_product_before == name_of_product_after
 
+    # test for adding products to cart
     def test_add_cart(self):
         self.test_valid_login("standard_user", "secret_sauce")
 
@@ -248,6 +264,7 @@ class Test_sauce:
 
         assert self.driver.current_url == constants.cart_url
 
+    # testing for twitter link
     def test_go_twitter(self):
         self.test_valid_login("standard_user", "secret_sauce")
         self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
@@ -263,6 +280,7 @@ class Test_sauce:
 
         assert self.driver.current_url == constants.twitter_url
 
+    # testing for facebook link
     def test_go_facebook(self):
         self.test_valid_login("standard_user", "secret_sauce")
         self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
@@ -278,6 +296,7 @@ class Test_sauce:
 
         assert self.driver.current_url == constants.facebook_url
 
+    # testing for linkedin link
     def test_go_linkedin(self):
         self.test_valid_login("standard_user", "secret_sauce")
         self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
@@ -293,6 +312,7 @@ class Test_sauce:
 
         assert self.driver.current_url == constants.linkedin_url
 
+    # testing to reset cart
     def test_reset_app_state(self):
         self.test_valid_login("standard_user", "secret_sauce")
 
@@ -316,6 +336,7 @@ class Test_sauce:
 
         assert cart_after.text == ""
 
+    # testing for cart link
     def test_go_to_cart(self):
         self.test_valid_login("standard_user", "secret_sauce")
 
@@ -328,6 +349,7 @@ class Test_sauce:
 
         assert self.driver.current_url == constants.cart_url
 
+    # testing continue button while in cart
     def test_cont_shopping(self):
         self.test_go_to_cart()
 
@@ -341,6 +363,7 @@ class Test_sauce:
 
         assert self.driver.current_url == constants.product_list_url
 
+    # Testing for checkout
     def test_checkout(self):
         self.test_go_to_cart()
 
@@ -353,22 +376,24 @@ class Test_sauce:
 
         assert self.driver.current_url == constants.checkout_step_one_url
 
-    def test_checkout_complete(self):
+    # testing for checkout complete
+    @pytest.mark.parametrize("first_name_in,last_name_in,zip_code_in", [("Kodlama,io", "Akcan", "42080")])
+    def test_checkout_complete(self, first_name_in, last_name_in, zip_code_in):
         self.test_checkout()
 
         self.wait_for_element_visible((By.ID, constants.checkout_first_name_id))
         first_name = self.driver.find_element(By.ID, constants.checkout_first_name_id)
-        first_name.send_keys("kodlama.io")
+        first_name.send_keys(first_name_in)
 
         self.wait_for_element_visible((By.ID, constants.checkout_last_name_id))
         last_name = self.driver.find_element(By.ID, constants.checkout_last_name_id)
         last_name.click()
-        last_name.send_keys("Akcan")
+        last_name.send_keys(last_name_in)
 
         self.wait_for_element_visible((By.ID, constants.checkout_zipcode_id))
         zip_code = self.driver.find_element(By.ID, constants.checkout_zipcode_id)
         zip_code.click()
-        zip_code.send_keys("42080")
+        zip_code.send_keys(zip_code_in)
 
         self.wait_for_element_visible((By.ID, constants.checkout_continue_btn_id))
         cont_btn2 = self.driver.find_element(By.ID, constants.checkout_continue_btn_id)
@@ -383,9 +408,9 @@ class Test_sauce:
 
         assert self.driver.current_url == constants.checkout_finished_url
 
+    # testing for back home button while in checkout
     def test_back_home(self):
-        self.test_checkout_complete()
-
+        self.test_checkout_complete("Kodlama,io", "Akcan", "42080")
         self.wait_for_element_visible((By.ID, constants.checkout_back_home_btn_id))
         self.driver.find_element(By.ID, constants.checkout_back_home_btn_id).click()
 
@@ -393,3 +418,154 @@ class Test_sauce:
         self.driver.save_screenshot(screenshot_file_path)
 
         assert self.driver.current_url == constants.product_list_url
+
+    # test checkout for empty continue
+    def test_checkout_empty_continue(self):
+        self.test_checkout()
+
+        self.wait_for_element_visible((By.ID, constants.checkout_continue_btn_id))
+        cont_btn2 = self.driver.find_element(By.ID, constants.checkout_continue_btn_id)
+        cont_btn2.click()
+
+        self.wait_for_element_visible((By.XPATH, constants.continue_error_messge_path))
+        error_message = self.driver.find_element(By.XPATH, constants.continue_error_messge_path)
+
+        screenshot_file_path = str(Path(self.folder_path) / f"test-empty-continue-click.png")
+        self.driver.save_screenshot(screenshot_file_path)
+
+        assert error_message.text == "Error: First Name is required"
+
+    # test checkout only first name for continue
+    @pytest.mark.parametrize("first_name_in", ["Kodlama,io"])
+    def test_checkout_only_first_name(self, first_name_in, ):
+        self.test_checkout()
+
+        self.wait_for_element_visible((By.ID, constants.checkout_first_name_id))
+        first_name = self.driver.find_element(By.ID, constants.checkout_first_name_id)
+        first_name.send_keys(first_name_in)
+
+        self.wait_for_element_visible((By.ID, constants.checkout_continue_btn_id))
+        cont_btn2 = self.driver.find_element(By.ID, constants.checkout_continue_btn_id)
+        cont_btn2.click()
+
+        self.wait_for_element_visible((By.XPATH, constants.continue_error_messge_path))
+        error_message = self.driver.find_element(By.XPATH, constants.continue_error_messge_path)
+
+        screenshot_file_path = str(Path(self.folder_path) / f"test-continuie-first name.png")
+        self.driver.save_screenshot(screenshot_file_path)
+
+        assert error_message.text == "Error: Last Name is required"
+
+    # test checkout firstname and lastname for continue
+    @pytest.mark.parametrize("first_name_in,last_name_in", [("Kodlama,io", "Akcan")])
+    def test_checkout_first_last_names(self, first_name_in, last_name_in):
+        self.test_checkout()
+
+        self.wait_for_element_visible((By.ID, constants.checkout_first_name_id))
+        first_name = self.driver.find_element(By.ID, constants.checkout_first_name_id)
+        first_name.send_keys(first_name_in)
+
+        self.wait_for_element_visible((By.ID, constants.checkout_last_name_id))
+        last_name = self.driver.find_element(By.ID, constants.checkout_last_name_id)
+        last_name.click()
+        last_name.send_keys(last_name_in)
+
+        self.wait_for_element_visible((By.ID, constants.checkout_continue_btn_id))
+        cont_btn2 = self.driver.find_element(By.ID, constants.checkout_continue_btn_id)
+        cont_btn2.click()
+
+        self.wait_for_element_visible((By.XPATH, constants.continue_error_messge_path))
+        error_message = self.driver.find_element(By.XPATH, constants.continue_error_messge_path)
+
+        screenshot_file_path = str(Path(self.folder_path) / f"test-continuie-first and last name.png")
+        self.driver.save_screenshot(screenshot_file_path)
+
+        assert error_message.text == "Error: Postal Code is required"
+
+    # test checkout firstname, zip code for continue
+    @pytest.mark.parametrize("first_name_in,zip_code_in", [("Kodlama,io", "42080")])
+    def test_checkout_firstname_zip(self, first_name_in, zip_code_in):
+        self.test_checkout()
+
+        self.wait_for_element_visible((By.ID, constants.checkout_first_name_id))
+        first_name = self.driver.find_element(By.ID, constants.checkout_first_name_id)
+        first_name.send_keys(first_name_in)
+
+        self.wait_for_element_visible((By.ID, constants.checkout_zipcode_id))
+        zip_code = self.driver.find_element(By.ID, constants.checkout_zipcode_id)
+        zip_code.click()
+        zip_code.send_keys(zip_code_in)
+
+        self.wait_for_element_visible((By.ID, constants.checkout_continue_btn_id))
+        cont_btn2 = self.driver.find_element(By.ID, constants.checkout_continue_btn_id)
+        cont_btn2.click()
+
+        self.wait_for_element_visible((By.XPATH, constants.continue_error_messge_path))
+        error_message = self.driver.find_element(By.XPATH, constants.continue_error_messge_path)
+
+        screenshot_file_path = str(Path(self.folder_path) / f"test-continuie-firtsname-zip-code.png")
+        self.driver.save_screenshot(screenshot_file_path)
+
+        assert error_message.text == "Error: Last Name is required"
+
+    # test checkout lastname, zip code for continue
+    @pytest.mark.parametrize("last_name_in,zip_code_in", [("Akcan", "42080")])
+    def test_checkout_lastname_zip(self, last_name_in, zip_code_in):
+        self.test_checkout()
+
+        self.wait_for_element_visible((By.ID, constants.checkout_last_name_id))
+        last_name = self.driver.find_element(By.ID, constants.checkout_last_name_id)
+        last_name.click()
+        last_name.send_keys(last_name_in)
+
+        self.wait_for_element_visible((By.ID, constants.checkout_zipcode_id))
+        zip_code = self.driver.find_element(By.ID, constants.checkout_zipcode_id)
+        zip_code.click()
+        zip_code.send_keys(zip_code_in)
+
+        self.wait_for_element_visible((By.ID, constants.checkout_continue_btn_id))
+        cont_btn2 = self.driver.find_element(By.ID, constants.checkout_continue_btn_id)
+        cont_btn2.click()
+
+        self.wait_for_element_visible((By.XPATH, constants.continue_error_messge_path))
+        error_message = self.driver.find_element(By.XPATH, constants.continue_error_messge_path)
+
+        screenshot_file_path = str(Path(self.folder_path) / f"test-continuie-lastname-zip-code.png")
+        self.driver.save_screenshot(screenshot_file_path)
+
+        assert error_message.text == "Error: First Name is required"
+    # test for sorting product from low to high
+    def test_lohi_filter(self):
+        self.test_valid_login("standard_user", "secret_sauce")
+        self.wait_for_element_visible((By.CLASS_NAME, constants.sort_btn_id))
+        dropdown = self.driver.find_element(By.CLASS_NAME, constants.sort_btn_id)
+
+        select = Select(dropdown)
+
+        select.select_by_value("lohi")
+
+        self.wait_for_element_visible((By.CLASS_NAME, "inventory_item_name"))
+        item_name = self.driver.find_element(By.CLASS_NAME, "inventory_item_name")
+
+        screenshot_file_path = str(Path(self.folder_path) / f"test-sorting-low-to-high.png")
+        self.driver.save_screenshot(screenshot_file_path)
+
+        assert item_name.text == "Sauce Labs Onesie"
+
+    # test for sorting product from high to low
+    def test_hilo_filter(self):
+        self.test_valid_login("standard_user", "secret_sauce")
+
+        self.wait_for_element_visible((By.CLASS_NAME, constants.sort_btn_id))
+        dropdown = self.driver.find_element(By.CLASS_NAME, constants.sort_btn_id)
+
+        select = Select(dropdown)
+
+        select.select_by_value("hilo")
+
+        self.wait_for_element_visible((By.CLASS_NAME, "inventory_item_name"))
+        item_name = self.driver.find_element(By.CLASS_NAME, "inventory_item_name")
+
+        screenshot_file_path = str(Path(self.folder_path) / f"test-sorting-high to low.png")
+        self.driver.save_screenshot(screenshot_file_path)
+        assert item_name.text == "Sauce Labs Fleece Jacket"
